cd0e8d72259f61e660cc1997f1a1123d6d970830
Jake <jakejohnowen@gmail.com>
2021-06-17 14:16:57 +0200

    spell error fix

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -39,14 +39,15 @@ const About = () => {
               <div className="about-wrapper__info">
                 <p className="about-wrapper__info-text">
                   {paragraphOne ||
-                    'I studied at Hyperion Dev and completed a 6 month course in software engineering. I then worked for 6 months as an intern for a startup called Sapphire Gem Tech who specialize in creating web applications. I spent lots of my time learning. Using that knowladge I built features for the companies portfolio websites. We worked mostly in Ruby on Rails.'}
+                    'I studied at Hyperion Dev and completed a 6 month course in software engineering. I then worked for 6 months as an intern for a startup called Sapphire Gem Tech who specialize in creating web applications. I spent lots of my time learning. Using that knowledge I built features for the companies portfolio websites. We worked mostly in Ruby on Rails.'}
                 </p>
                 <p className="about-wrapper__info-text">
                   {paragraphTwo ||
                     'As a software engineer I think my skill set is more on the back end. But I have come to find I really enjoy working on the front end and learning about design priniciples. I love the problem solving nature of programming and find the rewards of solving a problem incredibly satisfying.'}
                 </p>
                 <p className="about-wrapper__info-text">
-                  {paragraphThree || 'My most recent project has been the not-uberEats restuarant directory that I have brought to South Africa. This website lists all restaurants in cape town with independent delivery, saving restuarants and users money with the exlcusion of extra fees. I will be adding more areas to the website as it grows in popularity.'}
+                  {paragraphThree ||
+                    'My most recent project has been the not-uberEats restuarant directory that I have brought to South Africa. This website lists all restaurants in cape town with independent delivery, saving restuarants and users money with the exlcusion of extra fees. I will be adding more areas to the website as it grows in popularity.'}
                 </p>
                 {resume && (
                   <span className="d-flex mt-3">
